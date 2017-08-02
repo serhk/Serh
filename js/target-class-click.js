2b6f2ec2df2cd37abb5809259261c6404665c8b2
serhk <seerh@mail.ru>
2017-08-02 10:36:35 +0300

    Create target-class-click.js

---
+++ js/target-class-click.js
@@ -0,0 +1,5 @@
+document.getElementById('elem-id').addEventListener('click', (event) => {
+    if (event.target.classList.contains('js-item-class')) {
+        // здесь как-то обрабатываем
+    }
+});
